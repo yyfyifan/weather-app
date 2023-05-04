f715ae57e576be2d0dee274836d403bf911983cd
yyfyifan <yyfyifan@gmail.com>
2023-05-04 13:20:11 -0700

    Add some simple animations with react-spring

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import notFoundImage from "../assets/404.png";
 import { css } from "@emotion/react";
+import { useSpring, animated } from "@react-spring/web";
 
 interface Props {
   location: string;
 }
 
 const NotFound: React.FC<Props> = ({ location }) => {
+  const springs = useSpring({
+    from: { opacity: 0, scale: 0 },
+    to: { opacity: 1, scale: 1 },
+    delay: 100,
+    config: {
+      duration: 300,
+    },
+  });
+
   return (
-    <div
+    <animated.div
       css={css`
         width: 100%;
         text-align: center;
         margin-top: 50px;
-      `}>
+      `}
+      style={{ ...springs }}>
       <img
         src={notFoundImage}
         alt={"location not found"}
@@ -30,7 +41,7 @@ const NotFound: React.FC<Props> = ({ location }) => {
         `}>
         Oops! Invalid location : {location.toUpperCase()}
       </p>
-    </div>
+    </animated.div>
   );
 };
 NotFound.displayName = "NotFound";
--- src/components/WeatherBox.tsx
+++ src/components/WeatherBox.tsx
@@ -2,17 +2,27 @@ import { DataResponse } from "../interfaces/weather-data.ts";
 import React from "react";
 import WeatherDetails from "./WeatherDetails.tsx";
 import WeatherSummary from "./WeatherSummary.tsx";
+import { useSpring, animated } from "@react-spring/web";
 
 interface Props {
   data: DataResponse;
 }
 
 const WeatherBox: React.FC<Props> = ({ data }) => {
+  const springs = useSpring({
+    from: { opacity: 0, scale: 0 },
+    to: { opacity: 1, scale: 1 },
+    delay: 100,
+    config: {
+      duration: 300,
+    },
+  });
+
   return (
-    <div>
+    <animated.div style={{ ...springs }}>
       <WeatherSummary data={data} />
       <WeatherDetails data={data} />
-    </div>
+    </animated.div>
   );
 };
 WeatherBox.displayName = "WeatherBox";
